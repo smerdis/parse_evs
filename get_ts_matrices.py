a3846b4d72d28152c50e2933c34fb6be96658171
Arjun Mukerji <stormandstress@gmail.com>
2015-12-15 18:45:49 -0800

    dispatcher script for get_cond_timeseries() calls

---
+++ get_ts_matrices.py
@@ -0,0 +1,15 @@
+#!/usr/local/anaconda/bin/python
+import os
+
+tasks = ['EMOTION', 'GAMBLING', 'LANGUAGE', 'MOTOR', 'RELATIONAL', 'SOCIAL', 'WM']
+#tasks = ['EMOTION']
+#subjects = os.listdir('/home/despoB/connectome-data/')
+subjects = ['987983']
+for sub in subjects:
+	for task in tasks:
+		os.system("qsub -l mem_free=18G \
+			-N hcp_matrices\
+			-j y \
+			-o /home/despoB/arjun/parse_evs/sge/ \
+			-e /home/despoB/arjun/parse_evs/sge/ \
+			-V /home/despoB/arjun/parse_evs/parse_evs.py %s %s" %(sub,task))
